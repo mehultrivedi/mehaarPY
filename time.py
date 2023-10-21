0ae082ec2f46273063605d0844a7cdacbeba1cd7
mehultrivedi <hardi.trivedi@gmail.com>
2023-10-21 19:14:35 +0530

    Create time.py

---
+++ time.py
@@ -0,0 +1,9 @@
+import datetime
+
+e = datetime.datetime.now()
+
+print ("Current date and time = %s" % e)
+
+print ("Today's date:  = %s/%s/%s" % (e.day, e.month, e.year))
+
+print ("The time is now: = %s:%s:%s" % (e.hour, e.minute, e.second))
